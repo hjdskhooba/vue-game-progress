a9278e34a499ed9691d9f6d1c1b03b7fac77f091
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-05-11 23:16:40 +0600

    pull branch main

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,25 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Progress from "../components/Progress.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Progress,
+    path: '/',
+    name: 'home',
+    component: HomeView
   },
   {
-    path: "/document",
-    name: "document",
-    component: () =>
-      import(/* webpackChunkName: "docement" */ "../components/Document.vue"),
-  },
-];
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
