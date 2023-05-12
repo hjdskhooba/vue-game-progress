692f84b1f1b971eb8b7012a9a96f903469e1931d
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-05-12 15:59:24 +0600

    add:[progress.vue]: add progress bar & add controls

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
+<script setup></script>
+
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">My Progress</router-link> |
+    <router-link to="/document">Documentation</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -12,9 +14,21 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  width: 100%;
+  height: 100%;
   color: #2c3e50;
+  background-image: url("./assets/Modern\ -\ 1.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+  max-height: 100%;
+  overflow: hidden;
 }
-
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 nav {
   padding: 30px;
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Progress from "../components/Progress.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: Progress,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/document",
+    name: "document",
+    component: () =>
+      import(/* webpackChunkName: "docement" */ "../components/Document.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
